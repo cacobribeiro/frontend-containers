357a5e49d9c27c3c6b6bd09ca69ea35f5061cb09
Caio Ribeiro <cacobribeiro@gmail.com>
2021-10-25 17:53:51 -0300

    criação dos imports de pages

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from './Home';
+export { default as updateContainers } from './updateContainers';
+export { default as newContainer } from './newContainer';
+export { default as NotFound } from './NotFound';
