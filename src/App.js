3ed66b2cffca658794e2d3668b180b352c0b8a96
Caio Ribeiro <cacobribeiro@gmail.com>
2021-10-25 17:49:56 -0300

    app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './App.css';
+
+import * as Pages from './pages/index';
+import store from './redux/store';
+import Header from './Components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Header />
+      <div className="container-sm m-5 p-5">
+        <Switch>
+          <Route exact path="/update/:containerId" component={Pages.updateContainers} />
+          <Route exact path="/create" component={Pages.newContainer} />
+          <Route exact path="/home" component={Pages.Home} />
+          <Route component={Pages.NotFound} />
+        </Switch>
+      </div>
+    </Provider>
   );
 }
 
